b4866716dc3fa0b981b2dd02c93329b0bc5d819a
peter_alfie <alfie1104@gmail.com>
2021-09-29 08:48:22 +0900

    2.7~2.9 nicknames and room count are done

--- src/server.js
+++ src/server.js
@@ -18,58 +18,47 @@ const handleListen = () => console.log("Listening on http://localhost:3000");
 const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
+function publicRooms() {
+  const {
+    sockets: {
+      adapter: { sids, rooms },
+    },
+  } = wsServer;
+  const publicRooms = [];
+
+  rooms.forEach((_, key) => {
+    if (sids.get(key) === undefined) {
+      publicRooms.push(key);
+    }
+  });
+
+  return publicRooms;
+}
+
 wsServer.on("connection", (socket) => {
+  socket["nickname"] = "Anon";
   socket.onAny((event) => {
     console.log(`Socket Event:${event}`);
   });
   socket.on("enter_room", (roomName, done) => {
     socket.join(roomName);
     done();
-    socket.to(roomName).emit("welcome");
+    socket.to(roomName).emit("welcome", socket.nickname);
+    wsServer.sockets.emit("room_change", publicRooms());
   });
   socket.on("disconnecting", () => {
-    socket.rooms.forEach((room) => socket.to(room).emit("bye"));
+    socket.rooms.forEach((room) =>
+      socket.to(room).emit("bye", socket.nickname)
+    );
+  });
+  socket.on("disconnect", () => {
+    wsServer.sockets.emit("room_change", publicRooms());
   });
   socket.on("new_message", (msg, room, done) => {
-    socket.to(room).emit("new_message", msg);
+    socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
     done();
   });
+  socket.on("nickname", (nickname) => (socket["nickname"] = nickname));
 });
-/*
-//이렇게 함으로써 websocket 서버가 http서버 위에서 구동됨
-const wsServer = new WebSocket.Server({ httpServer });
-const sockets = [];
-
-wsServer.on("connection", (socket) => {
-  sockets.push(socket);
-  socket["nickname"] = "Anon";
-
-  //각 socket에 close, message 등의 이벤트 리스너를 등록함
-  //server에 등록하지 않았음에 유의!
-  console.log("Connected to Browser");
-
-  socket.on("close", () => {
-    console.log("Disconnected from the Browser : ❌");
-  });
-
-  socket.on("message", (msg) => {
-    const message = JSON.parse(msg);
-    switch (message.type) {
-      case "new_message":
-        sockets.forEach((aSocket) => {
-          aSocket.send(`${socket.nickname}: ${message.payload}`);
-        });
-        break;
-      case "nickname":
-        socket["nickname"] = message.payload;
-        break;
-      default:
-        console.log(message.payload);
-    }
-  });
-
-  socket.send(`${socket.nickname}: hello!`);
-});
-*/
 
 httpServer.listen(3000, handleListen);
